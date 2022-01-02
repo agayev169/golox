9085ca455769c6b3832daca3586dad03a5d31b26
Kamal Aghayev <kamal.aghayev@simbrella.com>
2022-01-02 04:01:17 +0400

    Added token and token type

---
+++ token.go
@@ -0,0 +1,63 @@
+package golox
+
+type TokenType int
+
+const (
+    NONE TokenType = iota
+
+	// Single-character tokens.
+	LEFT_PAREN
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	// One or two character tokens.
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	// Literals.
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	// Keywords.
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
+
+type Token struct {
+	Type    TokenType
+	Lexeme  string
+	Literal interface{}
+	Line    int
+	Col     int
+}
