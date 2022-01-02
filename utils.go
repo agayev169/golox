64491ed5be143852a2f62277eaa8c9232808b63d
Kamal Aghayev <kamal.aghayev@simbrella.com>
2022-01-02 04:01:28 +0400

    Added utility functions

---
+++ utils.go
@@ -0,0 +1,41 @@
+package golox
+
+import (
+	"fmt"
+	"log"
+)
+
+func fatal(err error) {
+	if err == nil {
+		return
+	}
+
+	// log.Printf("[ERR] %s\n", err)
+	panic(fmt.Sprintf("[ERR] %s\n", err))
+}
+
+func warn(err error) {
+	if err == nil {
+		return
+	}
+
+	log.Printf("[WARN]: %s\n", err)
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func isAlpha(b byte) bool {
+	return (b >= 'a' && b <= 'z') ||
+		(b >= 'A' && b <= 'Z') ||
+		b == '_'
+}
+
+
+func isAlphaNumeric(b byte) bool {
+	return (b >= 'a' && b <= 'z') ||
+		(b >= 'A' && b <= 'Z') ||
+		(b >= '0' && b <= '9') ||
+		b == '_'
+}
