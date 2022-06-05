ee929b78bd092a6223b2111af60b233bee8831ee
Kamal Aghayev <kamal.aghayev@simbrella.com>
2022-06-05 16:43:48 +0400

    Add token names map and String method

--- token.go
+++ token.go
@@ -1,5 +1,7 @@
 package golox
 
+import "fmt"
+
 type TokenType int
 
 const (
@@ -54,6 +56,52 @@ const (
 	EOF
 )
 
+var tokenNames = map[TokenType]string{
+	LEFT_PAREN:  "(",
+	RIGHT_PAREN: ")",
+	LEFT_BRACE:  "{",
+	RIGHT_BRACE: "}",
+	COMMA:       ",",
+	DOT:         ".",
+	MINUS:       "-",
+	PLUS:        "+",
+	SEMICOLON:   ";",
+	SLASH:       "/",
+	STAR:        "*",
+
+	BANG:          "!",
+	BANG_EQUAL:    "!=",
+	EQUAL:         "=",
+	EQUAL_EQUAL:   "==",
+	GREATER:       ">",
+	GREATER_EQUAL: ">=",
+	LESS:          "<",
+	LESS_EQUAL:    "<=",
+
+	IDENTIFIER: "IDENTIFIER",
+	STRING:     "STRING",
+	NUMBER:     "NUMBER",
+
+	AND:    "and",
+	CLASS:  "class",
+	ELSE:   "else",
+	FALSE:  "false",
+	FUN:    "fun",
+	FOR:    "for",
+	IF:     "if",
+	NIL:    "nil",
+	OR:     "or",
+	PRINT:  "print",
+	RETURN: "return",
+	SUPER:  "super",
+	THIS:   "this",
+	TRUE:   "true",
+	VAR:    "var",
+	WHILE:  "while",
+
+	EOF: "EOF",
+}
+
 type Token struct {
 	Type    TokenType
 	Lexeme  string
@@ -61,4 +109,8 @@ type Token struct {
 	File    string
 	Line    int
 	Col     int
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("(Token): { type: '%s'; lexeme: '%s'; loc: %s:%d:%d}", tokenNames[t.Type], t.Lexeme, t.File, t.Line, t.Col)
 }
